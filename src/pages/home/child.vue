68a71170d7bfb2a5d6c11717f350f624bf334377
zhubaoda01 <zhubaoda01@lianjia.com>
2018-01-05 15:09:23 +0800

    父子组件绑定事件

---
+++ src/pages/home/child.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+  	<button @click="add">点击增加</button>
+    <span>{{count}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home-resource',
+  data () {
+    return {
+      
+    }
+  },
+  props:{
+  	count:{
+  		type:Number
+  	}
+  },
+  methods:{
+  	add(){
+  		//可以理解发广播告诉父组件我被点击了，点击事件名字叫add
+  		this.$emit('add')
+  	}
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/pages/home/resource.vue
+++ src/pages/home/resource.vue
@@ -1,19 +1,26 @@
 <template>
   <div>
-    资源页
+  	<!--count是传给子组件的值，addNum绑定子组件触发的事件-->
+    <child @add="addNum" :count="count"></child>
   </div>
 </template>
 
 <script>
+import child from './child.vue'
 export default {
   name: 'home-resource',
   data () {
     return {
-      
+      count:0
     }
   },
   methods:{
-  	
+  	addNum(){
+  		this.count++;
+  	}
+  },
+  components:{
+    	   child
   }
 }
 </script>
